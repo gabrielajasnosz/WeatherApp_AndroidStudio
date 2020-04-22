ca22a441dc0f9832e24a517b5c85f0b515347333
gjasnosz <gabriela.jasnosz@gmail.com>
2020-04-22 14:38:52 +0200

    activity2

--- app/src/main/java/com/example/weather_app/MainActivity.java
+++ app/src/main/java/com/example/weather_app/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.weather_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button b1=findViewById(R.id.checkButton);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent=new Intent(MainActivity.this,display_weather.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/weather_app/display_weather.java
@@ -0,0 +1,16 @@
+package com.example.weather_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class display_weather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_weather);
+    }
+}
