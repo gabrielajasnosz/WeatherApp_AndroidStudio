d745287ce4b9ea7c56831980ca84209d2a75b145
gjasnosz <gabriela.jasnosz@gmail.com>
2020-05-04 16:47:40 +0200

    lab9

--- app/src/main/java/com/example/weather_app/DisplayWeather.java
+++ app/src/main/java/com/example/weather_app/DisplayWeather.java
@@ -1,36 +1,50 @@
 package com.example.weather_app;
 
 import android.annotation.SuppressLint;
-
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Gravity;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.squareup.picasso.Picasso;
 import java.util.Calendar;
 import java.util.TimeZone;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class DisplayWeather extends AppCompatActivity {
     private MainWeather weather;
 
-   TextView time,tempMax,tempMin,humidity,cityTemp,cityPressure,cityName,feels_like;
+   TextView time,tempMax,tempMin,humidity,cityTemp,cityPressure,cityName,feels_like,info;
+   SwipeRefreshLayout swipe;
     private String iconUrl = "https://openweathermap.org/img/wn/";
     ImageView image;
+    String city;
+    Toast error;
+
 
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_weather);
-
         Intent intent = getIntent();
-        String city = intent.getStringExtra("CITY_NAME");
+        city = intent.getStringExtra("CITY_NAME");
         weather = (MainWeather) intent.getSerializableExtra("WEATHER_CLASS");
-
-        image=findViewById(R.id.imageView);
         cityName=findViewById(R.id.cityName);
+        image=findViewById(R.id.imageView);
         cityPressure=findViewById(R.id.cityPressure);
         cityTemp=findViewById(R.id.cityTemp);
         time=findViewById(R.id.time);
@@ -38,22 +52,102 @@ public class DisplayWeather extends AppCompatActivity {
         tempMin=findViewById(R.id.cityTempMin);
         humidity=findViewById(R.id.cityHumidity);
         feels_like=findViewById(R.id.cityFeelsLike);
+        swipe = findViewById(R.id.swipeRefresh);
+        info=findViewById(R.id.dataInfo);
+        cityName.setText(city.toUpperCase());
+        update(weather);
+
+        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                if(network()) {
+                    refreshData();
+                    swipe.setRefreshing(false);
+                }
+                else{
+                    error = Toast.makeText(getApplicationContext(), "No internet connection!", Toast.LENGTH_SHORT);
+                    error.setGravity(Gravity.TOP, 0, 200);
+                    error.show();
+                }
+            }
+        });
+        Thread t=new Thread(){
+            public void run(){
+                try{
+                    while(!isInterrupted()){
+                        Thread.sleep(60000);
+                        runOnUiThread(new Runnable(){
+                            public void run(){
+                                if(network()) {
+                                    refreshData();
+                                }
+                                else{
+                                    error = Toast.makeText(getApplicationContext(), "No internet connection!", Toast.LENGTH_SHORT);
+                                    error.setGravity(Gravity.TOP, 0, 200);
+                                    error.show();
+
+                                }
+                            }
+                        });
+                    }
+                } catch(InterruptedException e){
+                    System.out.println("ERROR");
+                }
+            }
+        };
+        t.start();
+    }
+
+
+    private void refreshData() {
+        Call<MainWeather> call = MainActivity.findData(city);
+        call.enqueue(new Callback<MainWeather>() {
+            @Override
+            public void onResponse(Call<MainWeather> call, Response<MainWeather> response) {
+                weather=response.body();
+                update(weather);
+                error = Toast.makeText(getApplicationContext(), "Data refreshed.", Toast.LENGTH_SHORT);
+                error.setGravity(Gravity.TOP, 0, 200);
+                error.show();
+            }
+            @Override
+            public void onFailure(Call<MainWeather> call, Throwable t) {
+                System.out.println("error");
+            }
+        });
+    }
+
+
+    public void update(MainWeather weather){
         iconUrl = iconUrl + weather.getWeather()[0].getIcon() + "@2x.png";
-
-
         Picasso.with(this).load(iconUrl).error(R.drawable.error).into(image);
         TimeZone tz = TimeZone.getDefault();
         Calendar c = Calendar.getInstance(tz);
         @SuppressLint("DefaultLocale") String time3 = String.format("%02d", c.get(Calendar.HOUR_OF_DAY)) + ":" +
                 String.format("%02d", c.get(Calendar.MINUTE));
         time.setText(time3);
-        cityName.setText(city.toUpperCase());
         cityTemp.setText(weather.getMain().getTemp()+ " °C");
         cityPressure.setText(weather.getMain().getPressure() + " hPa");
         tempMax.setText(weather.getMain().getTemp_max()+" °C");
         tempMin.setText(weather.getMain().getTemp_min()+" °C");
         humidity.setText(weather.getMain().getHumidity()+" %");
         feels_like.setText(weather.getMain().getFeels_like()+ " °C");
+
     }
 
-}
+    public boolean network(){
+        boolean wifi = false;
+        boolean mobile = false;
+        ConnectivityManager connManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mMobile = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (mWifi.isConnected()) {
+            wifi = true;
+        }
+        if(mMobile.isConnected()){
+            mobile = true;
+        }
+        return wifi||mobile;
+    }
+
+    }
--- app/src/main/java/com/example/weather_app/MainActivity.java
+++ app/src/main/java/com/example/weather_app/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.weather_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.UnsupportedEncodingException;
 import android.view.View;
+import android.widget.Toast;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,13 +24,14 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    private final String apiid = "749561a315b14523a8f5f1ef95e45864";
-    private final String units = "metric";
+    private String apiid = "749561a315b14523a8f5f1ef95e45864";
+    private String units = "metric";
     Button b1;
     TextView errorMsg;
     EditText cityName;
     private MainWeather weather;
     private Intent intent;
+    Toast error;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,33 +44,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendText(View view) {
-        final String cityNameText = cityName.getText().toString();
-        intent = new Intent(this, DisplayWeather.class);
-        intent.putExtra("CITY_NAME", cityNameText);
+        if(network()) {
+            final String cityNameText = cityName.getText().toString();
+            intent = new Intent(this, DisplayWeather.class);
+            intent.putExtra("CITY_NAME", cityNameText);
 
-        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.openweathermap.org/").
-                addConverterFactory(GsonConverterFactory.create()).build();
-        JsonPlaceHolderAPI jsonPlaceHolderAPI = retrofit.create(JsonPlaceHolderAPI.class);
+            Call<MainWeather> call = findData(cityNameText);
+            call.enqueue(new Callback<MainWeather>() {
+                @Override
+                public void onResponse(Call<MainWeather> call, Response<MainWeather> response) {
+                    if (!response.isSuccessful()) {
+                        errorMsg.setText("No information.");
+                        return;
+                    }
+                    weather = response.body();
+                    intent.putExtra("WEATHER_CLASS", weather);
+                    startActivity(intent);
+                    saveData(cityNameText);
 
-        Call<MainWeather> call = jsonPlaceHolderAPI.getMainWeatherClassCall(cityNameText + ",pl", apiid, units);
-        call.enqueue(new Callback<MainWeather>() {
-            @Override
-            public void onResponse(Call<MainWeather> call, Response<MainWeather> response) {
-                if (!response.isSuccessful()) {
-                    errorMsg.setText("No information.");
-                    return;
                 }
-                weather = response.body();
-                intent.putExtra("WEATHER_CLASS", weather);
-                startActivity(intent);
-                saveData(cityNameText);
 
-            }
-            @Override
-            public void onFailure(Call<MainWeather> call, Throwable t) {
-                System.out.println(t.getMessage());
-            }
-        });
+                @Override
+                public void onFailure(Call<MainWeather> call, Throwable t) {
+                    System.out.println(t.getMessage());
+                }
+            });
+        }
+        else{
+            error = Toast.makeText(getApplicationContext(), "No internet connection!", Toast.LENGTH_SHORT);
+            error.setGravity(Gravity.TOP, 0, 200);
+            error.show();
+        }
     }
 
     public void saveData(String output){
@@ -78,5 +88,33 @@ public class MainActivity extends AppCompatActivity {
         String name= sharedPreferences.getString("REMEMBERED_NAME","City");
         cityName.setText(name);
     }
+
+    public static Call<MainWeather> findData(String city){
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.openweathermap.org/").
+                addConverterFactory(GsonConverterFactory.create()).build();
+
+        JsonPlaceHolderAPI jsonPlaceholderAPI = retrofit.create(JsonPlaceHolderAPI.class);
+
+        Call<MainWeather> call = jsonPlaceholderAPI.getMainWeatherClassCall(city+",pl", "749561a315b14523a8f5f1ef95e45864","metric" );
+
+        return call;
+
+    }
+
+    public boolean network(){
+        boolean wifi = false;
+        boolean mobile = false;
+        ConnectivityManager connManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mMobile = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (mWifi.isConnected()) {
+            wifi = true;
+        }
+        if(mMobile.isConnected()){
+            mobile = true;
+        }
+        return wifi||mobile;
+    }
+
 
 }
